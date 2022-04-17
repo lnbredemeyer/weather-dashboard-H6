d49bbd3ec9da63d6876fce86494662f6db1808c0
lnbredemeyer <99215212+lnbredemeyer@users.noreply.github.com>
2022-04-16 23:23:13 -0500

    updates

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var key = 'bad4c6fb514de50e1ba64cb13ca925e8';
+var cities = [];
+var history = [];
+var pastHistory = $('.history');
+var date = moment().format('dddd, MMMM Do YYYY');
+var time = moment().format('YYYY-MM-DD HH:MM:SS');
